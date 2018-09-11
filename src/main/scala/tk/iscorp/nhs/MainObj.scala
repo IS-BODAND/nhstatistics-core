865fc17617aff2f542c0093964b465aae3785bc9
bodand <bodand@pm.me>
2018-09-11 23:19:43 +0200

    Logger instance and up-to-date MainObj

--- src/main/scala/tk/iscorp/nhs/MainObj.scala
+++ src/main/scala/tk/iscorp/nhs/MainObj.scala
@@ -1,10 +1,14 @@
 package tk.iscorp.nhs
 
-import tk.iscorp.nhs.datagetter.HttpGetter
+import tk.iscorp.nhs.datagetter.{HtmlResponseProcessor, HttpGetter}
 
 object MainObj {
   def main(args: Array[String]): Unit = {
     val httpClientWrapper = new HttpGetter
+    val htmlResponseProcessor = new HtmlResponseProcessor
     val httpResponse = httpClientWrapper.getHttpFromUri("https://nhentai.net/g/228922")
+    val semiDummyGallery = htmlResponseProcessor.processHtmlToGallery(httpResponse)
+
+    println(semiDummyGallery.toString)
   }
 }
---
+++ src/main/scala/tk/iscorp/nhs/Utils.scala
@@ -0,0 +1,7 @@
+package tk.iscorp.nhs
+
+import org.slf4j.{Logger, LoggerFactory}
+
+object Utils {
+  val logger: Logger = LoggerFactory.getLogger("GenericNHStatistics")
+}
