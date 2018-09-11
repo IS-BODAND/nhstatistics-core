ed32ed0efd69e4d7cf81c9e1ebaa9ff6cb8c14f9
bodand <bodand@pm.me>
2018-09-11 23:20:28 +0200

    existentials import removed

--- src/main/scala/tk/iscorp/nhs/data/hentai/HentaiGroup.scala
+++ src/main/scala/tk/iscorp/nhs/data/hentai/HentaiGroup.scala
@@ -2,7 +2,10 @@ package tk.iscorp.nhs.data.hentai
 
 import org.jetbrains.annotations.{NonNls, NotNull}
 
+import scala.language.existentials
+
 class HentaiGroup(@NonNls @NotNull override val name: String,
                   @NotNull override val amount: Int) extends HentaiData {
 
 }
+
--- src/main/scala/tk/iscorp/nhs/data/hentai/HentaiParody.scala
+++ src/main/scala/tk/iscorp/nhs/data/hentai/HentaiParody.scala
@@ -2,6 +2,8 @@ package tk.iscorp.nhs.data.hentai
 
 import org.jetbrains.annotations.{NonNls, NotNull}
 
+import scala.language.existentials
+
 class HentaiParody(@NonNls @NotNull override val name: String,
                    @NotNull override val amount: Int) extends HentaiData {
 
