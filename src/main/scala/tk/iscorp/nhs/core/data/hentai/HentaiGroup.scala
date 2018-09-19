6b3e2c945b063705d60a33b93223ab213af2fde8
bodand <bodand@pm.me>
2018-09-19 22:23:48 +0200

    finished ScalaDoc on all public classes and methods

--- src/main/scala/tk/iscorp/nhs/core/data/hentai/HentaiGroup.scala
+++ src/main/scala/tk/iscorp/nhs/core/data/hentai/HentaiGroup.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ InfoSoft OpenSource Licence
+
+ Copyright (c) 2018.
+
+ Permission is hereby granted to absolutely free usage of this software in any way
+ that doesn't conflict with the the licensee's local laws. Modification and
+ redistribution of this software is permitted, but the changes must be stated, and
+ the source software (this one) must be stated. Redistributed versions must be
+ licensed under the InfoSoft OpenSource Licence. Projects using a (modified or not)
+ version of this software, may or may not use the InfoSoft OpenSource Licence.
+ Commercial distribution is permitted. This licence must be made available to the
+ end user from within the program, and to all programmers from a IS-OSL.LICENCE.txt file.
+ Inclusion of the licence in the source file(s) may be used instead of the IS-OSL.LICENCE.txt file.
+
+ ******************************************************************************/
+
 package tk.iscorp.nhs.core.data.hentai
 
 import org.jetbrains.annotations.{NonNls, NotNull}
@@ -8,6 +25,16 @@ import java.util.{HashMap ⇒ JMap}
 import scala.language.existentials
 import scala.xml.Node
 
+/**
+  * HentaiGroup tag. Represents one group with at least one doujin in them.
+  *
+  * @param name   Name of the group.
+  * @param amount Amount of doujin in the group
+  *
+  * @author bodand
+  *
+  * @since 1.0
+  */
 class HentaiGroup(@NonNls @NotNull override val name: String,
                   @NotNull override val amount: Int) extends HentaiData {
   override def toXml: Node = <group name={s"$name"} amount={s"$amount"} />
--- src/main/scala/tk/iscorp/nhs/core/data/hentai/HentaiLanguage.scala
+++ src/main/scala/tk/iscorp/nhs/core/data/hentai/HentaiLanguage.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ InfoSoft OpenSource Licence
+
+ Copyright (c) 2018.
+
+ Permission is hereby granted to absolutely free usage of this software in any way
+ that doesn't conflict with the the licensee's local laws. Modification and
+ redistribution of this software is permitted, but the changes must be stated, and
+ the source software (this one) must be stated. Redistributed versions must be
+ licensed under the InfoSoft OpenSource Licence. Projects using a (modified or not)
+ version of this software, may or may not use the InfoSoft OpenSource Licence.
+ Commercial distribution is permitted. This licence must be made available to the
+ end user from within the program, and to all programmers from a IS-OSL.LICENCE.txt file.
+ Inclusion of the licence in the source file(s) may be used instead of the IS-OSL.LICENCE.txt file.
+
+ ******************************************************************************/
+
 package tk.iscorp.nhs.core.data.hentai
 
 import org.jetbrains.annotations.NotNull
@@ -7,6 +24,18 @@ import java.util.{HashMap ⇒ JMap}
 
 import scala.xml.Node
 
+/**
+  * Represents the language of the doujin.
+  * Can be on of the following:
+  * <ul>
+  * <li>English - [[EnglishHentai]]</li>
+  * <li>Japanese - [[JapaneseHentai]]</li>
+  * <li>Chinese - [[ChineseHentai]]</li>
+  * <li>Translated - [[TranslatedHentai]] (I still don't see the point in this tag)</li>
+  * <li>Rewrite - [[RewriteHentai]]</li>
+  * </ul>
+  * [[OtherLanguageHentai]] exists so we don't get murdered by something fucky.
+  */
 sealed abstract case class HentaiLanguage() extends HentaiData {
   override def toXml: Node = <language name={s"$name"} amount={s"$amount"} />
 
