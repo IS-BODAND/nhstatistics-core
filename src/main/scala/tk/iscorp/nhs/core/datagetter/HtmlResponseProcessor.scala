2e95c408867ec670085c46939e747e39ed11d856
bodand <bodand@pm.me>
2018-09-19 23:26:56 +0200

    rise of the chaos wizards

--- src/main/scala/tk/iscorp/nhs/core/datagetter/HtmlResponseProcessor.scala
+++ src/main/scala/tk/iscorp/nhs/core/datagetter/HtmlResponseProcessor.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ InfoSoft OpenSource Licence
+
+ Copyright (c) 2018.
+
+ Permission is hereby granted to absolutely free usage of this software in any way
+ that doesn't conflict with the the licensee's local laws. Modification and
+ redistribution of this software is permitted, but the changes must be stated, and
+ the source software (this one) must be stated. Redistributed versions must be
+ licensed under the InfoSoft OpenSource Licence. Projects using a (modified or not)
+ version of this software, may or may not use the InfoSoft OpenSource Licence.
+ Commercial distribution is permitted. This licence must be made available to the
+ end user from within the program, and to all programmers from a IS-OSL.LICENCE.txt file.
+ Inclusion of the licence in the source file(s) may be used instead of the IS-OSL.LICENCE.txt file.
+
+ ******************************************************************************/
+
 package tk.iscorp.nhs.core.datagetter
 
 import org.jsoup.Jsoup
@@ -30,7 +47,7 @@ private[core] class HtmlResponseProcessor {
          .toInt
     } catch {
       case _: NullPointerException ⇒
-        Utils.logger.error("=> Error getting hentai id. Most likely means that it was deleted. " +
+        Utils.logger.error("√-1 => Error getting hentai id. Most likely means that it was deleted. " +
                               s"Continuing to try, don't expect much.")
         fault += 1
         0
@@ -41,8 +58,8 @@ private[core] class HtmlResponseProcessor {
       document.selectFirst("div#info h1").childNode(0).toString
     } catch {
       case _: NullPointerException ⇒
-        Utils.logger.error(s"$id => Error getting hentai name. Most likely means that it was deleted. " +
-                              s"Continuing to try, don't expect much.")
+        Utils.logger.error(s"${if (id != 0) id else "√-1"} => Error getting hentai name. Most likely means that it " +
+                              s"was deleted. Continuing to try, don't expect much.")
         fault += 1
         "!![[ERROR GETTING DOUJIN NAME]]!!"
     }
@@ -50,7 +67,7 @@ private[core] class HtmlResponseProcessor {
       document.selectFirst("div#info h2").childNode(0).toString
     } catch {
       case _: NullPointerException ⇒
-        Utils.logger.info("Gallery has no secondary/japanese name")
+        Utils.logger.info(s"$id => Gallery has no secondary/japanese name")
         ""
     }
 
