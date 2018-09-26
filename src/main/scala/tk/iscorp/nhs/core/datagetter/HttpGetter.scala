6ec295923e6b9526d9cfbf0e281b6e51db4da8a9
bodand <bodand@pm.me>
2018-09-26 21:07:26 +0200

    nhentai is pure https

--- src/main/scala/tk/iscorp/nhs/core/datagetter/HttpGetter.scala
+++ src/main/scala/tk/iscorp/nhs/core/datagetter/HttpGetter.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ InfoSoft OpenSource Licence
+
+ Copyright (c) 2018.
+
+ Permission is hereby granted to absolutely free usage of this software in any way
+ that doesn't conflict with the the licensee's local laws. Modification and
+ redistribution of this software is permitted, but the changes must be stated, and
+ the source software (this one) must be stated. Redistributed versions must be
+ licensed under the InfoSoft OpenSource Licence. Projects using a (modified or not)
+ version of this software, may or may not use the InfoSoft OpenSource Licence.
+ Commercial distribution is permitted. This licence must be made available to the
+ end user from within the program, and to all programmers from a IS-OSL.LICENCE.txt file.
+ Inclusion of the licence in the source file(s) may be used instead of the IS-OSL.LICENCE.txt file.
+
+ ******************************************************************************/
+
 package tk.iscorp.nhs.core.datagetter
 
 import org.apache.commons.validator.routines.UrlValidator
@@ -12,7 +29,7 @@ private[core] class HttpGetter() {
   @NotNull
   private val httpClient: CloseableHttpClient = HttpClients.createDefault()
   @NotNull
-  private val urlSchemes: Array[String] = Array("http", "https")
+  private val urlSchemes: Array[String]       = Array("https")
 
   @NotNull
   @NonNls
